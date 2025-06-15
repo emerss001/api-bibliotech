d59ef90862c41adb99de8ac9d09bbb26c100e21e
emerss001 <emersonnevess80@gmail.com>
2025-06-15 13:41:48 -0300

    ajustando conexão com o banco de dados e inicialização do Firebase para usar variáveis de ambiente

--- src/db/ConnectionDB.java
+++ src/db/ConnectionDB.java
@@ -1,17 +1,18 @@
 package db;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionDB {
-    private static final String url = "jdbc:mysql://localhost:3306/acervoinclusivo";
-    private static final String user = "root";
-    private static final String password = "74569";
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String url = "jdbc:mysql://api-bibliotech-emersonnevess80.k.aivencloud.com:11667/defaultdb"
+            + "?useSSL=true&requireSSL=true&verifyServerCertificate=false"; // se estiver testando
+    private static final String user = dotenv.get("DB_USER");
+    private static final String password = dotenv.get("DB_PASSWORD");
 
-//    private static final String url = System.getenv("DB_URL");
-//    private static final String user = System.getenv("DB_USER");
-//    private static final String password = System.getenv("DB_PASSWORD");
     public static Connection getConnection() {
         try {
             return DriverManager.getConnection(url, user, password);
